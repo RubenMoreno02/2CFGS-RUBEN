6ab9761a65d86584d452db845dfe02e888cc2afd
Ruben <Ruben@gmail.com>
2025-11-06 17:42:40 +0100

    Final

--- DIW/MaquetWeb/script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript para mostrar/ocultar el menú al hacer clic en la hamburguesa
-document.getElementById('hamburger-btn').addEventListener('click', function() {
-    // Selecciona el menú de navegación
-    var menu = document.querySelector('.nav-menu');
-
-    // Añade o elimina la clase "active" para mostrar o esconder el menú
-    menu.classList.toggle('active');
-});
