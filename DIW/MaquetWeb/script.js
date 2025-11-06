253623dd0d0be75166bee0fbcf1fd5cc657bb9ed
RubenMoreno02 <rubmorsis@alu.edu.gva.es>
2025-11-06 09:55:03 +0100

    CAMBIOS

--- DIW/MaquetWeb/script.js
+++ DIW/MaquetWeb/script.js
@@ -1,5 +1,8 @@
-// Función para alternar el menú en pantallas pequeñas
-function toggleMenu() {
-    const menu = document.querySelector('.nav-menu');
-    menu.classList.toggle('open');
-}
+// JavaScript para mostrar/ocultar el menú al hacer clic en la hamburguesa
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+    // Selecciona el menú de navegación
+    var menu = document.querySelector('.nav-menu');
+
+    // Añade o elimina la clase "active" para mostrar o esconder el menú
+    menu.classList.toggle('active');
+});
