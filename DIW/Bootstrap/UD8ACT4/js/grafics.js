fbc6ac277c60123de59c512eca264b6fdf0629ea
RubenMoreno02 <rubmorsis@alu.edu.gva.es>
2026-05-04 13:04:23 +0200

    dd

--- DIW/Bootstrap/UD8ACT4/js/grafics.js
+++ DIW/Bootstrap/UD8ACT4/js/grafics.js
@@ -2,19 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     new Chart(document.getElementById("pie-chart"), {
         type: 'pie',
         data: {
-            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            labels: ["Africa", "Asia", "Amèrica"],
             datasets: [{
                 label: "Population (millions)",
                 backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                data: [2478, 5267, 734, 784, 433]
+                data: [150, 300, 50]
             }]
         },
         options: {
-            plugins: {
-                title: {
-                    display: true,
-                    text: 'Predicted world population (millions) in 2050'
-                }
+            plugins: {               
             }
         }
     });
