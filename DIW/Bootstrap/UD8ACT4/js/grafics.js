0897567181ee9524b1520094d9c1df5329b4acc0
RubenMoreno02 <rubmorsis@alu.edu.gva.es>
2026-05-05 10:05:20 +0200

    Dia 5

--- DIW/Bootstrap/UD8ACT4/js/grafics.js
+++ DIW/Bootstrap/UD8ACT4/js/grafics.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
             labels: ["Africa", "Asia", "Amèrica"],
             datasets: [{
                 label: "Population (millions)",
-                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f"],
                 data: [150, 300, 50]
             }]
         },
